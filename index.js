392c404361bf5c94934e525fbe0ea2e5712834fd
Andre Braga <dnk_vip@hotmail.com>
2018-10-19 10:38:41 -0300

    Foi feito modificacao na api

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res){
-  res.send('hello world');
+app.get('/', (req, res) => {
+  res.json('André Braga - API');
 });
 
 var port = process.env.PORT || 1337;
