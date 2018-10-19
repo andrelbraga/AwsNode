0283c84058754018dc90c71a367637ea4e935515
Andre Braga <dnk_vip@hotmail.com>
2018-10-19 10:35:39 -0300

    Alteracao para express

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-var http = require('http');
+var express = require('express');
+var app = express();
 
-var server = http.createServer(function(request, response) {
-
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Bannn");
-
+app.get('/', function(req, res){
+  res.send('hello world');
 });
 
 var port = process.env.PORT || 1337;
-server.listen(port);
-
-console.log("Server running at http://localhost:%d", port);
+app.listen(port);
