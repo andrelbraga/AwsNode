616ae1513089447c814a0cd429b91279861cb39f
Andre Braga <dnk_vip@hotmail.com>
2018-10-19 10:47:21 -0300

    Andre API

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-  res.json({'André Braga - API'});
+  res.json({API: 'André Braga - API'});
 });
 
 var port = process.env.PORT || 1337;
